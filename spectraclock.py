d91cc133934a26ae6a337ee22dbbc9e152f8923a
mattopia <matt@podsix.org>
2020-04-02 17:19:33 -0400

    Add KeyboardInterrupt exception catch, cosmetic fixes.

--- spectraclock.py
+++ spectraclock.py
@@ -7,7 +7,7 @@ import time
 import re
 
 def parseArgs():
-    p = argparse.ArgumentParser(description='Control an attached Symmetricom clock.')
+    p = argparse.ArgumentParser(description='Synchronize an attached Spectracom clock.')
     p.add_argument('dev', help='Serial device (i.e. /dev/ttyAMA0)')
     p.add_argument('--debug', action='store_true', help='Enable debug output.')
     p.add_argument('--utc', action='store_true', help='Output UTC time instead of local time.')
@@ -15,7 +15,7 @@ def parseArgs():
     return p.parse_args()
 
 def formatTime(t, ss = " "):    
-    # Return binary encoded time in Symmetricom Format 1
+    # Return binary encoded time in Spectracom Format 1
     # t = Time to return formatted, in struct_time format
     # ss = Sync status; " " (space) indicates good, ? and * indicate bad
     day = time.strftime("%a", t)
@@ -32,12 +32,16 @@ def sendTime(ser, unsync = False, utc = False, debug = False):
         print(msg)
 
 def main(args = parseArgs()):
-    ser = serial.Serial(args.dev, 9600)
-    s = sched.scheduler(time.time, time.sleep)
-    while True:
-        s.enterabs(int(time.time()) + 1, 1, sendTime, 
-            kwargs={'ser': ser, 'unsync': args.unsync, 'utc': args.utc, 'debug': args.debug})
-        s.run()
+    try:
+        ser = serial.Serial(args.dev, 9600)
+        s = sched.scheduler(time.time, time.sleep)
+        while True:
+            s.enterabs(int(time.time()) + 1, 1, sendTime, 
+                kwargs={'ser': ser, 'unsync': args.unsync, 'utc': args.utc, 'debug': args.debug})
+            s.run()
+    except KeyboardInterrupt:
+        print("Exiting...")
+        ser.close
 
 if __name__ == "__main__":
     main()
