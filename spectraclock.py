b75b0dfc492cfaf6693b18d9ca88e9ee05cc9d4a
mattopia <matt@podsix.org>
2020-04-05 10:30:35 -0400

    Add option to set NTP check interval

--- spectraclock.py
+++ spectraclock.py
@@ -12,12 +12,17 @@ def parseArgs():
     p.add_argument('dev', help='Serial device (i.e. /dev/ttyAMA0)')
     p.add_argument('--debug', action='store_true', help='Enable debug output.')
     p.add_argument('--utc', action='store_true', help='Output UTC time instead of local time.')
+    p.add_argument('--ntp', type=int, default=60, help='Check NTP interval, zero to disable.')
     return p.parse_args()
 
-def checkNtpSync():
-    p = subprocess.Popen(['chronyc', '-c', 'tracking'], stdout=subprocess.PIPE)
+def checkNtpSync(debug = False):
+    p = subprocess.Popen(['chronyc', '-c', 'tracking'], 
+        stdout=subprocess.PIPE, stderr=subprocess.PIPE)
     try:
         output, errors = p.communicate(timeout = 0.1)
+        if debug:
+            print(f'chrony stdout: {output}')
+            print(f'chrony stderr: {errors}')
     except subprocess.TimeoutExpired:
         print("NTP Check - Timeout Exceeded")
         p.kill()
@@ -26,7 +31,7 @@ def checkNtpSync():
     offset = float(output.decode().split(",")[4])
     if offset > 0.1 or offset < -0.1:
         return False
-    if status != "00000000" and p.returncode == 0 and errors == None:
+    if status != "00000000" and p.returncode == 0:
         return True
     else:
         return False
@@ -51,20 +56,21 @@ def sendTime(ser, sync = True, utc = False, debug = False):
 
 def main(args = parseArgs()):
     try:
-        sync = False
-        c = 30
+        sync = True
         ser = serial.Serial(args.dev, 9600)
+        c = args.ntp
         s = sched.scheduler(time.time, time.sleep)
         while True:
-            s.enterabs(int(time.time()) + 1, 1, sendTime, 
-                kwargs={'ser': ser, 'sync': sync, 'utc': args.utc, 'debug': args.debug})
-            s.run()
-            if c > 15:
-                sync = checkNtpSync()
+            if args.ntp > 0:
+                c += 1
+            if c > args.ntp:
+                sync = checkNtpSync(args.debug)
                 if args.debug:
                     print(f"NTP status: {sync}")
                 c = 0
-            c += 1
+            s.enterabs(int(time.time()) + 1, 1, sendTime, 
+                kwargs={'ser': ser, 'sync': sync, 'utc': args.utc, 'debug': args.debug})
+            s.run()
     except KeyboardInterrupt:
         print("Exiting...")
         ser.close
